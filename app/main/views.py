69fd63177d0edda96c23bc0e1cb11ef237215bdc
Liz Matee <lizziematee@gmail.com>
2018-04-23 00:28:21 +0300

    add all changes

--- app/main/views.py
+++ app/main/views.py
@@ -4,3 +4,11 @@ from flask import Flask, render_template
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/aboutme')
+def about():
+    return render_template('about.html')
+
+@main.route('/end')
+def end():
+    return render_template('end.html')
