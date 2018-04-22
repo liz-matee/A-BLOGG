07b5be5ce57f6c583dfdb0eeb0d64033d671d79f
Liz Matee <lizziematee@gmail.com>
2018-04-23 02:37:10 +0300

    regester.html added

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,5 @@
 from . import main
-from flask import Flask, render_template,flash,redirect,url_for,session,logging
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
 from wtforms import Form, striigfield, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 
@@ -15,3 +14,17 @@ def about():
 @main.route('/end')
 def end():
     return render_template('end.html')
+
+class RegisterForm(Form):
+    name = striigfield('Name', [validators.Length(min=2, max=50)])
+    username = striigfield('username' [validators.Length(min=4, max=26)])
+    email = striigfield('email', [validators.Length(min=2, max=40)])
+    password = PasswordField('password',[ validators.DataRequired(),validators.EqualTo('confirm', message='passwords do not match')])
+    confirm = PasswordField('confirm password')
+
+@main.route('/register', methords=['GET','POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.methord == 'POST'an form.validate():
+        render_template('regester.html')
+    render_template('regester.html', form=form)
